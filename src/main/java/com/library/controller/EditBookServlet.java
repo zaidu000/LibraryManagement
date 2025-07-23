e96777cc47fa35a944e9219cc2705159ae37e186
ParasJain12 <parasjain8103@gmail.com>
2025-07-23 18:37:34 +0530

    Create Book model class, also create dminDashboard and studentDashboard.jsp files

---
+++ src/main/java/com/library/controller/EditBookServlet.java
@@ -0,0 +1,16 @@
+package com.library.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditBookServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+}
---
+++ src/main/java/com/library/model/Book.java
@@ -0,0 +1,59 @@
+package com.library.model;
+
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+    private String edition;
+    private int quantity;
+    private String parkingSlot;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getParkingSlot() {
+        return parkingSlot;
+    }
+
+    public void setParkingSlot(String parkingSlot) {
+        this.parkingSlot = parkingSlot;
+    }
+    
+}
