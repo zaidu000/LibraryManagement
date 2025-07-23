e5c6127adb8200e6bdaf37cd16404f1a14aff969
ParasJain12 <parasjain8103@gmail.com>
2025-07-23 18:55:10 +0530

    Create AddBookServlet & DeleteBookServlet for admin

--- src/main/java/com/library/controller/AddBookServlet.java
+++ src/main/java/com/library/controller/AddBookServlet.java
@@ -1,6 +1,9 @@
 package com.library.controller;
 
+import com.library.utility.DBConnection;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +14,23 @@ public class AddBookServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        String name = request.getParameter("name");
+        String author = request.getParameter("author");
+        String edition = request.getParameter("edition");
+        int quantity = Integer.parseInt(request.getParameter("quantity"));
+        String parkingSlot = request.getParameter("parkingSlot");
+        try(Connection con = DBConnection.getConnection()){
+            PreparedStatement ps = con.prepareStatement("insert into book(name,author,edition,quantity,parkingSlot) values(?,?,?,?,?)");
+            ps.setString(1, name);
+            ps.setString(2, author);
+            ps.setString(3, edition);
+            ps.setInt(4, quantity);
+            ps.setString(5,parkingSlot);
+            ps.executeUpdate();
+            response.sendRedirect("viewBooks.jsp");
+        }catch(Exception e){
+            request.setAttribute("error", "Error: "+e.getMessage());
+            request.getRequestDispatcher("adminDashboard.jsp").forward(request, response);
+        }
     }
 }
--- src/main/java/com/library/controller/DeleteBookServlet.java
+++ src/main/java/com/library/controller/DeleteBookServlet.java
@@ -1,6 +1,9 @@
 package com.library.controller;
 
+import com.library.utility.DBConnection;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +14,15 @@ public class DeleteBookServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        int id = Integer.parseInt(request.getParameter("id"));
+        try(Connection con = DBConnection.getConnection()){
+            PreparedStatement ps = con.prepareStatement("delete from book where id=?");
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            response.sendRedirect("viewBooks.jsp");
+        }catch(Exception e){
+            request.setAttribute("error", "Error: "+e.getMessage());
+            request.getRequestDispatcher("adminDashboard.jsp").forward(request, response);
+        }
     }
 }
